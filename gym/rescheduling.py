d4b46646606e35a0d40e0a16878e7ff6b827e6cd
Till Sahm <till.sahm@dfki.de>
2025-11-14 11:25:03 +0100

    export generate streams

--- gym/rescheduling.py
+++
@@ -1,7 +0,0 @@
-# TODO: wtf is this voodoo?
-import sys
-sys.path.append("/home/tisc06/RESI-TSN-DFKI/tsnscheduler")
-from scheduler import GracuniasScheduler
-
-if __name__ == "__main__":
-    print("pass")
--- scheduler/__init__.py
+++ scheduler/__init__.py
@@ -2,5 +2,6 @@ from .business import Link, Network, Node, Offset, Stream
 from .constants import MAX_MTU_SIZE_IN_BYTES
 from .specific import HermesScheduler, GracuniasScheduler, TSNSchedScheduler
 from .file import read_network_from_csv, read_offsets_from_file, write_offsets_to_file, read_streams_from_csv, write_streams_to_csv
+from .streams import generate_streams
 
-__all__ = ["business", "constants", "specific", "file"]
+__all__ = ["business", "constants", "specific", "file", "streams"]
